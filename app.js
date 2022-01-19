a87d5cd19336e6e35be47c74ef5b168398878640
Viraj Jain <virajjain7.vj@gmail.com>
2022-01-19 17:37:24 +0530

    update function
    function that converts the date from number to string.  Also takes care of '0' when day/month is less than 10

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ function reverseStr(str) {
   function checkPalindrome(str) {
     return (str === reverseStr(str));
   }
+  function convertDateToStr(date) {
+    var dateStr = {
+      day: '',
+      month: '',
+      year: ''
+    };
+    if (date.day < 10) {
+      dateStr.day = '0' + date.day;
+    }
+    else {
+      dateStr.day = date.day.toString();
+    }
+    if (date.month < 10) {
+      dateStr.month = '0' + date.month;
+    }
+    else {
+      dateStr.month = date.month.toString();
+    }
+    dateStr.year = date.year.toString();
+  return dateStr;
+  }
